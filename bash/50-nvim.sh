75f00b0dbc3c9526986ffbd18de01b6df266e647
Thomas Merx <11958162+thomasmerx@users.noreply.github.com>
2023-10-30 08:14:24 +0100

    create symlinks and add to path for lsp servers

---
+++ bash/50-nvim.sh
@@ -0,0 +1,23 @@
+create_link() {
+	pushd ~/.local/share/nvim/mason/bin/vscode || exit
+	filename=$(basename -- "$1")
+	destination=$(find "$1" -type l -printf "%l")
+	destination=${destination/tom/vscode}
+	if [ ! -f "$filename" ]; then
+		ln -s "$destination" "$filename"
+	fi
+	popd || exit
+}
+create_links() {
+	for symlink in "$@"; do
+		create_link "$symlink"
+	done
+}
+export -f create_link
+export -f create_links
+
+if [ ! -d "$HOME/.local/share/nvim/mason/bin/vscode" ]; then
+	mkdir -p "$HOME/.local/share/nvim/mason/bin/vscode"
+fi
+find ~/.local/share/nvim/mason/bin -maxdepth 1 -type l -exec bash -c 'create_links "$@"' bash {} +
+echo PATH="$PATH":~/nvim/bin:~/.local/share/nvim/mason/bin/vscode>> ~/.bashrc
