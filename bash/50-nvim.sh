f0aac3a0d0488414982401f8b9cd7baea86402e4
Thomas Merx <11958162+thomasmerx@users.noreply.github.com>
2023-11-13 07:56:47 +0100

    create global symlink from /home/tom/.local /home/vscode/.local

--- bash/50-nvim.sh
+++ bash/50-nvim.sh
@@ -1,23 +1,10 @@
-create_link() {
-	pushd ~/.local/share/nvim/mason/bin/vscode || exit
-	filename=$(basename -- "$1")
-	destination=$(find "$1" -type l -printf "%l")
-	destination=$(echo "$destination" | sed -e "s#/home/[a-z]*/#/home/vscode/#")
-	if [ ! -f "$filename" ]; then
-		ln -s "$destination" "$filename"
-	fi
-	popd || exit
-}
-create_links() {
-	for symlink in "$@"; do
-		create_link "$symlink"
-	done
-}
-export -f create_link
-export -f create_links
-
-if [ ! -d "$HOME/.local/share/nvim/mason/bin/vscode" ]; then
-	mkdir -p "$HOME/.local/share/nvim/mason/bin/vscode"
+id >> /tmp/profile.log
+if [ ! -f "/home/tom/.local" ]; then
+	sudo ln -s "$HOME/.local" /home/tom/.local
+	echo "ln result: $?" >> /tmp/profile.log
+else
+	echo "ln not done" >> /tmp/profile.log
 fi
-find ~/.local/share/nvim/mason/bin -maxdepth 1 -type l -exec bash -c 'create_links "$@"' bash {} +
-echo PATH="$PATH":~/nvim/bin:~/.local/share/nvim/mason/bin/vscode>> ~/.bashrc
+echo alias vim='nvim' >> ~/.bashrc
+echo alias sudo='sudo ' >> ~/.bashrc
+echo PATH="$PATH":~/nvim/bin >> ~/.bashrc
