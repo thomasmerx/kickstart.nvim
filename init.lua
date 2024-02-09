405ac7edf7f6059767e58848468cc45fb7a7e061
Thomas Merx <11958162+thomasmerx@users.noreply.github.com>
2024-02-09 16:41:19 +0100

    enable custom plugins

--- init.lua
+++ init.lua
@@ -276,6 +276,7 @@ require('lazy').setup({
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
