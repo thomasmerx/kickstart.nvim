0388726e58ec04b6e38b10987aabcd4ba3ffdb5e
Thomas Merx <11958162+thomasmerx@users.noreply.github.com>
2024-06-27 11:24:31 +0200

    enable nix usage in devcontainer

--- bash/50-nvim.sh
+++ bash/50-nvim.sh
@@ -5,13 +5,24 @@ if [ ! -f "/home/tom/.local" ]; then
 else
 	echo "ln not done" >> /tmp/profile.log
 fi
+if [ ! -f "$HOME/.nix-profile" ]; then
+	sudo ln -s "$HOME/.local/state/nix/profiles/profile" "$HOME/.nix-profile"
+	echo "ln nix result: $?" >> /tmp/profile.log
+else
+	echo "ln nix not done" >> /tmp/profile.log
+fi
 
-grep "/nvim/bin" ~/.bashrc > /dev/null
+grep "/nix/var" ~/.profile > /dev/null
+if [ $? == 1 ]; then
+	echo PATH='$PATH':/nix/var/nix/profiles/default/bin >> ~/.profile
+	echo PATH='$PATH':/home/vscode/.nix-profile/bin >> ~/.profile
+	echo PATH='$PATH':~/nvim/bin >> ~/.profile
+	echo PATH="$PATH":/home/vscode/nvim/bin | sudo tee -a /etc/environment
+fi
+grep "alias vim" ~/.bashrc > /dev/null
 if [ $? == 1 ]; then
 	echo alias sudo=\'sudo -s --preserve-env=PATH \' >> ~/.bashrc
 	echo alias vim=\'nvim\' >> ~/.bashrc
 	echo alias gis=\'git status\' >> ~/.bashrc
-	echo PATH="$PATH":~/nvim/bin >> ~/.bashrc
-	echo PATH="$PATH":/home/vscode/nvim/bin | sudo tee -a /etc/environment
 	echo export SHELL=/bin/bash >> ~/.bashrc
 fi
