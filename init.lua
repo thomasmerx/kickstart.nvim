bcb9e1aa8a6057207e55b1ec4cee7ff51131fb04
Thomas Merx <11958162+thomasmerx@users.noreply.github.com>
2024-11-13 11:51:13 +0100

    add render-markdown

--- init.lua
+++ init.lua
@@ -304,6 +304,16 @@ require('lazy').setup({
     opts = {} -- this is equalent to setup({}) functions
   },
 
+  {
+      'MeanderingProgrammer/render-markdown.nvim',
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+      ---@module 'render-markdown'
+      ---@type render.md.UserConfig
+      opts = {},
+  },
+
   { 'windwp/nvim-ts-autotag' },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
