dac8b675f8f67d26bd87a573d818593f7ae67ed9
Thomas Merx <11958162+thomasmerx@users.noreply.github.com>
2025-03-04 09:27:38 +0100

    add dooku plugin for automatic html docu generation

--- init.lua
+++ init.lua
@@ -325,22 +325,28 @@ require('lazy').setup({
   },
 
   {
-      'MeanderingProgrammer/render-markdown.nvim',
-      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-      ---@module 'render-markdown'
-      ---@type render.md.UserConfig
-      opts = {},
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
   },
 
   { 'windwp/nvim-ts-autotag' },
 
   {
-      'danymat/neogen',
-      config = true,
-      -- Uncomment next line if you want to follow only stable versions
-      -- version = "*"
+    'danymat/neogen',
+    config = true,
+    -- Uncomment next line if you want to follow only stable versions
+    -- version = "*"
+  },
+
+  {
+    'Zeioth/dooku.nvim',
+    event = "VeryLazy",
+    opts = {},
   },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
