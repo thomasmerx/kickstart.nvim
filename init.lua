d8424528d1266584500611d17280bbe54137563a
Thomas Merx <11958162+thomasmerx@users.noreply.github.com>
2025-05-23 10:14:35 +0200

    change mason url and pin versions

--- init.lua
+++ init.lua
@@ -114,8 +114,8 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
+      { 'mason-org/mason.nvim', config = true, version = '^1.1.0' },
+      {'mason-org/mason-lspconfig.nvim', version = '^1.32.0' },
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
